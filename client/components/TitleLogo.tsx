7757dfaa9bdceaec25318adc6b5f0570fa428498
ghulammf <muhtadinamri123@gmail.com>
2026-01-14 20:06:01 +0700

    create title and logo components

---
+++ client/components/TitleLogo.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+function TitleLogo(){
+    return(
+        <div className="flex flex-col items-center text-white mb-10">
+            <div className="w-20 h-20 bg-white rounded-[20px] my-auto mx-5 flex items-center justify-center text-4xl">🎓</div>
+            <h1 className="text-[2.8em] mb-4 shadow-2xl">Smart Cluster Analytics</h1>
+            <p className="text-[1.2em] opacity-95 max-w-175 my-0 mx-auto">Sistem Analisis Clustering Mahasiswa Baru Berbasis Data Mining untuk Optimalisasi Pengelompokan dan Pengambilan Keputusan Akademik</p>
+        </div>
+    )
+}
+
+export default TitleLogo
