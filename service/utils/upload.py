3d417b56c0d583c33f9a32112a1f21e9e5d5aa86
ghulammf <muhtadinamri123@gmail.com>
2026-01-18 22:10:53 +0700

    add remove existing file function

--- service/utils/upload.py
+++ service/utils/upload.py
@@ -14,7 +14,20 @@ def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@upload_bp.route('/upload', methods=['POST'])
+def clear_upload_folder(folder_path):
+    for filename in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, filename)
+        try:
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+        except Exception as e:
+            return jsonify({
+                "status": "Error",
+                "message": "Gagal menyimpan data",
+                "Error": e
+            })
+
+@upload_bp.route('/api/upload', methods=['POST'])
 def upload_file():
     if 'file' not in request.files:
         return jsonify({"error": "File tidak ditemukan"}), 400
@@ -26,8 +39,14 @@ def upload_file():
 
     if not allowed_file(file.filename):
         return jsonify({"error": "Format file tidak diizinkan"}), 400
+    
+    # -- Menghapus data lama
+    if os.path.exists(UPLOAD_FOLDER):
+        clear_upload_folder(UPLOAD_FOLDER)
 
-    filename = secure_filename(file.filename)
+    _, ext = os.path.splitext(file.filename)
+    ext = ext.lower()
+    filename = secure_filename(f"data_fitur{ext}")
     save_path = os.path.join(UPLOAD_FOLDER, filename)
     file.save(save_path)
 
