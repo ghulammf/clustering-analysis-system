afb3475d319daae4289bfedfdcd06946fdd100a4
ghulammf <muhtadinamri123@gmail.com>
2026-01-20 22:09:47 +0700

    add messages status running to frontend

--- service/src/preprocessing/cleaning.py
+++ service/src/preprocessing/cleaning.py
@@ -8,21 +8,22 @@ class DataCleaning:
         self.file_path = file_path
         self.df = None
         self.cleaned_df = None
+        self.messages =[]
 
     def load_data(self):
         """Load data from csv"""
         try:
             self.df = pd.read_csv(self.file_path)
-            print(f"Data berhasil dimuat. Shape: {self.df.head}")
-            # print(self.df.head())
+            self.messages.append(f"Memuat data fitur")
+            return self.df
         except FileNotFoundError:
-            print(f"File {self.file_path} tidak ditemukan")
+            self.messages.append(f"File {self.file_path} tidak ditemukan")
             return None
         
     def select_features(self):
         """Select features based on requirements"""
         if self.df is None:
-            print("Data belum dimuat")
+            self.messages.append("Data belum dimuat")
             return None
         
         selected_features = [
@@ -37,20 +38,27 @@ class DataCleaning:
         # filter hanya kolom yang ada di dataset
         available_features = [col for col in selected_features if col in self.df.columns]
         self.df = self.df[available_features]
-        print(f"Selected {len(available_features)} features")
+
+        # Filter tahun angkatan 2023
+        if 'TAHUN ANGKATAN' in self.df.columns:
+            self.df = self.df[self.df['TAHUN ANGKATAN'] == 2023]
+        else:
+            self.messages.append("Data mahasiswa tahun 2023 tidak ditemukan")
+
+        self.messages.append(f"Selected {len(available_features)} features")
         return self.df
     
     def handle_missing_values(self):
         """Handle missing values in the dataset"""
         if self.df is None:
-            print("Data belum dimuat!")
+            self.messages.append("Data belum dimuat!")
             return None
         
-        print("Menangani missing values...")
+        self.messages.append("Menangani missing values")
 
         # Informasi missing values sebelum cleaning
         missing_before = self.df.isnull().sum().sum()
-        print(f"Total missing values sebelum cleaning: {missing_before}")
+        # self.messages.append(f"Total missing values sebelum cleaning: {missing_before}")
 
         # 1. Kolom IPK, ganti NaN dengan 0
         ipk_columns = [col for col in self.df.columns if 'IPK' in col]
@@ -70,16 +78,17 @@ class DataCleaning:
 
         # Missing values after cleaning
         missing_after = self.df.isnull().sum().sum()
-        print(f"Total missing values setelah cleaning: {missing_after}")
+        # self.messages.append(f"Total missing values setelah cleaning: {missing_after}")
+        # self.messages.append(f"Total missing values: sebelum: {missing_before}, sesudah: {missing_after}")
         return self.df
     
     def handle_outliers(self):
         """Handle outliers in numerical columns"""
         if self.df is None:
-            print("Data belum dimuat")
+            self.messages.append("Data belum dimuat")
             return None
         
-        print("Menangani outliers...")
+        self.messages.append("Menangani outliers")
 
         # Only process numerical columns that are not IDs or similar
         numerical_cols = self.df.select_dtypes(include=[np.number]).columns
@@ -98,15 +107,15 @@ class DataCleaning:
                 self.df[col] = np.where(self.df[col] < lower_bound, lower_bound, self.df[col])
                 self.df[col] = np.where(self.df[col] > upper_bound, upper_bound, self.df[col])
 
-                return self.df
+        return self.df
             
     def create_ipk_rata_rata(self):
         """Create IPK_RATA_RATA columns for all IPK columns"""
         if self.df is None:
-            print("Data belum dimuat")
+            self.messages.append("Data belum dimuat")
             return None
                 
-        print("Membuat kolom IPK_RATA_RATA...")
+        print("Membuat kolom IPK_RATA_RATA")
 
         # Find all IPK columns
         ipk_columns = [col for col in self.df.columns if 'IPK' in col]
@@ -116,17 +125,17 @@ class DataCleaning:
             ipk_data_replaced = ipk_data.replace(0, np.nan)
             self.df['IPK_RATA_RATA'] = ipk_data_replaced.mean(axis = 1)
             self.df['IPK_RATA_RATA']= self.df['IPK_RATA_RATA'].fillna(0)
-            print(f"Kolom IPK_RATA_RATA berhasil dibuat. Rata-rata: {self.df['IPK_RATA_RATA'].mean():.2f}")
+            # self.messages.append(f"Kolom IPK_RATA_RATA berhasil dibuat. Rata-rata: {self.df['IPK_RATA_RATA'].mean():.2f}")
         else:
-            print("Tidak ditemukan kolom IPK")
+            self.messages.append("Tidak ditemukan kolom IPK")
             self.df['IPK_RATA_RATA'] = 0
 
-        return self.df 
+        return self.df
 
     def finalize_selected_features(self):
         """Select final features as specified"""
         if self.df is None:
-            print("Data belum dimuat!")
+            self.messages.append("Data belum dimuat!")
             return None
                 
         final_features = ['ASAL_PROVINSI', 'ASAL_KOTA', 'KERJA_AYAH', 'PENGHASILAN_KATEGORI',
@@ -137,21 +146,51 @@ class DataCleaning:
         existing_features = [col for col in final_features if col in self.df.columns]
         self.cleaned_df = self.df[existing_features].copy()
 
-        print(f"Final features: {existing_features}")
-        print(f"Final dataset shape: {self.clened_df.shape}")
+        # self.messages.append(f"Final features: {existing_features}")
+        self.messages.append(f"Final dataset shape: {self.cleaned_df.shape}")
 
         return self.cleaned_df
     
-    def run_cleaning(self):
-        print("MEMULAI DATA CLEANING")
+    def run_cleaning(self, return_result=False):
+        self.messages.append("MEMULAI DATA CLEANING")
 
-        self.load_data()
+        df = self.load_data()
+        if df is None:
+            return self.response()
+
         self.select_features()
         self.handle_missing_values()
         self.handle_outliers()
         self.create_ipk_rata_rata()
-        self.finalize_selected_features()
+        result = self.finalize_selected_features()
 
-if __name__ == "__main__":
-    cleaner = DataCleaning()
-    cleaned_data = cleaner.run_cleaning()
+        print("clenaing selesai")
+
+        if return_result:
+            return result
+
+        return self.response()
+    
+    def response(self):
+        return{
+            "status": "success",
+            "messages": self.messages,
+            "data_preview": self.cleaned_df.head(5).to_dict(orient="records")
+        }
+    # def run_ws(self):
+    #     yield self._step("load_data", "memuat data", self.load_data)
+    #     yield self._step("select_features", "Menyeleksi fitur", self.select_features)
+    #     yield self._step("handle_missing_values", "Menangani missing values", self.handle_missing_values)
+    #     yield self._step("handle_outliers", "Menangani outliers", "berhasil Menangani outliers")
+    #     yield self._step("create_ipk_rata-rata", "Membuat rata-rata IPK", self.create_ipk_rata_rata)
+    #     yield self._step("finalize_selected_features", "Finalisasi data", "Berhasil melakukan Finalisasi data")
+
+    # def _step(self, key, label, func):
+    #     result = func()
+    #     return {
+    #         "key": key,
+    #         "label": label,
+    #         "message": result
+    #     }
+
+   
