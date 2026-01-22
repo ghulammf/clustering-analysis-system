0287b51bcb6e84de1f7da1c113c29dd9ad8b623b
ghulammf <muhtadinamri123@gmail.com>
2026-01-22 22:48:30 +0700

    add json safety response modul

---
+++ service/utils/to_json_safe.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+def to_json_safe(obj):
+    if isinstance(obj, dict):
+        return {k: to_json_safe(v) for k, v in obj.items()}
+    elif isinstance(obj, list):
+        return [to_json_safe(i) for i in obj]
+    elif isinstance(obj, np.integer):
+        return int(obj)
+    elif isinstance(obj, np.floating):
+        return float(obj)
+    else:
+        return obj
